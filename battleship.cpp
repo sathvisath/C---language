6db946ac7a38ea1793568208295f88ea09073984
sathvisath <143781899+sathvisath@users.noreply.github.com>
2025-02-05 22:50:54 +0530

    Add files via upload

---
+++ battleship.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main(){
+    bool ships[4][4]={
+        {0,1,0,1},
+        {0,0,1,0},
+        {1,0,0,0},
+        {0,0,0,1}
+    };
+    int hits=0;
+    int numberofturns=0;
+    while(hits<5){
+        int row,column;
+
+        // selecting coordinates
+        cout<<"Selecting coordinates.\n";
+
+        //ask user row number
+        cout<<"Enter the row number: ";
+        cin>>row;
+
+        //ask user column number
+        cout<<"Enter the column number: ";
+        cin>>column;
+
+        if(ships[row][column]){
+            ships[row][column]=0;
+            hits++;
+            cout<<"Still"<<(4-hits)<<"left.\n";
+        }else{
+            cout<<"You missed!";
+        }
+
+        numberofturns++;
+    }
+    cout<<"You won!!!";
+    cout<<"You won in"<<numberofturns<<".";
+}
