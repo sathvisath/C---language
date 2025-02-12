48a3b9d1a9a99107bc76625682323d633c47c0fa
sathvisath <143781899+sathvisath@users.noreply.github.com>
2025-02-12 19:25:48 +0530

    Add files via upload

---
+++ function1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//passing arguments
+void function(string name){
+    cout<<"My name is "<<name<<"\n";
+}
+
+int main(){
+    function("Liam");
+    function("Sathvi");
+    function("Lucky");
+    function("Kamal");
+    function("Yuva");
+}
