cdd37e763ed73ff8e963e9d352a520314176f68e
sathvisath <143781899+sathvisath@users.noreply.github.com>
2025-02-12 19:28:07 +0530

    function1.cpp

--- function1.cpp
+++ function1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
-//passing arguments
+//passing parameters , here name is the parameter
 void function(string name){
     cout<<"My name is "<<name<<"\n";
 }
 
+//values to that parameters are known as arguments
 int main(){
     function("Liam");
     function("Sathvi");
